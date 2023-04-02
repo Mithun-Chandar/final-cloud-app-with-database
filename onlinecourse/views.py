2e266ad489960c3d79e63285125537c7d7c410ed
Mithun <Mithunchandar16@gmail.com>
2023-04-02 19:35:43 -0400

    Final Changes V2

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -149,6 +149,6 @@ def show_exam_result(request, course_id, submission_id):
         context['course'] = course
         context['question_grade'] = exam_score
         context['choices'] = choices
-    return render(request, 'onlinecourse/exam_result_bootstrap.html', context=context)
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 
